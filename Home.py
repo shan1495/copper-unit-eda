0fe47c15d788e4985fa6201a7c9fe19a6be3335e
Shan <kuttysince85@gmail.com>
2023-12-07 12:02:08 +0530

    branch creation and init commit

---
+++ Home.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Home",
+    page_icon=":derelict_house_building:",
+)
+st.sidebar.info("select the menu")
+st.markdown("## :green[Welcome to Industrial Copper Modeling]")
+st.markdown("Technologies used")
+st.markdown("Python")
+st.markdown("Pandas")
+st.markdown("Streamlit")
+st.markdown("Github")
+
