5953f1ac79454da3cef61b51a9ffb94e874173fb
Shan <kuttysince85@gmail.com>
2023-12-15 00:15:13 +0530

    Update 1_Skewness.py

--- pages/1_Skewness.py
+++ pages/1_Skewness.py
@@ -2,32 +2,36 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sb
+import utils.utility as util
 
 st.markdown("Sknewness finding")
 st.write("Sknewness shows how the data is distributed. It should be normally distributed always.")
-with st.form(key='load-data'):
-    clicked = st.form_submit_button(label='Load Data')
-    if clicked:
-        with st.spinner("Loading the data"):
-            
-            df = pd.read_excel('.\\dataset\\Copper_Set.xlsx',dtype='unicode')
-           
-            #df = df.groupby(['delivery date']).count().reset_index()
-        fig,ax = plt.subplots()
-        df = df.drop(df.query('item_date == "19950000"').index)
-        print("Deleting the 19950000 data .. and 1919 data...")
-        df = df.drop(df.query('item_date== "20191919"').index)
-        df = df.drop(df.query('`delivery date` == "30310101"').index)
-        df = df.drop(df.query('`delivery date` == "20212222"').index)
-        df = df.drop(df.query('status !="Won" & status !="Loss"').index)
-        st.dataframe(data=df)
-        df['item_date']=pd.to_datetime(df['item_date'], format='mixed').dt.date
-        df['delivery date']=pd.to_datetime(df['delivery date'],format='mixed').dt.date
-        df.drop('material')
-            
-        sb.histplot(data=df,x='delivery date',binwidth=10,kde=True,bins=20, label="Skewness: %.2f"%(df["selling_price"].skew()))
-        df['country'].mode()
-        df['country'].fill
-        #sb.lineplot(data=df,x='delivery date',y='selling_price')
-        plt.figure(fig)
-        st.write(fig)
+df = st.session_state['cleaned_dataframe']
+st.write(df.select_dtypes('number').skew(axis=0))
+st.write("With the above data, it is evident that the quantity tons, thickness, selling_price attributes are positive skewness. \n")
+st.write("Skewness = 0: Then normally distributed.\nSkewness > 0: Then more weight in the left tail of the distribution.\n Skewness < 0: Then more weight in the right tail of the distribution.")
+
+with st.form('graph-form'):
+    grp = st.form_submit_button("View Graph")
+    plt.figure(figsize=(17,13))
+    #sb.histplot(data = df['thickness'], kde=True)
+    option = st.selectbox("Select Atrribute",options=['selling_price','quantity tons','thickness','width'])
+#     if option:
+#         with st.spinner("We are generating graph. Wait"):
+#             f,ax = plt.subplots()
+#             print("Option -->",option)
+#             sb.histplot(df,x=option, kde=True,ax=ax)      
+#     #sb.histplot(data = df['quantity tons'], kde=True)   
+#             st.write(f)
+# # f,ax = plt.subplots()
+# x=df[['quantity tons','application','thickness','width','selling_price','country','customer','item_type']].corr()
+# sb.heatmap(x, annot=True, cmap="YlGnBu")
+# st.write(f)
+    cols = ['selling_price','quantity tons','thickness','width']
+    fix = st.form_submit_button('Fix Skewness')
+    if fix:
+        df = util.test_log_transforms(cols,df)
+        st.session_state['final_df'] = df
+
+    st.markdown("After fixing the skewing issue.. Now")
+    st.write(df.select_dtypes('number').skew(axis=0))
